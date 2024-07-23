cc72ca79716c8da902166b7e09a639d90e04bc79
sfinke543 <sebastian.jonas.finke@web.de>
2024-07-23 18:10:57 +0200

    Add files via upload

---
+++ Belkendiagreamm mit Fehlerindikatoren.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+# Beispiel-Daten für drei Gruppen mit je drei Datensätzen
+data = {
+    '50µg': [0.075602925, 0.15541231, 0.069624064],
+    '10µg': [0.128292806, 0.282267517, 0.072351241],
+    '5µg': [0.145376532, 0.379271934, 0.05967667],
+    '1µg': [0.485804989, 1.604964478, 0.229284026]
+}
+
+
+
+
+
+labels = list(data.keys())
+values = [np.mean(data[label]) for label in labels]
+errors = [np.std(data[label]) for label in labels]
+colors = ['blue', 'orange', 'red', 'green']
+
+# Erstellen des Balkendiagramms
+plt.figure(figsize=(10, 6))
+bars = plt.bar(np.arange(len(labels)), values, yerr=errors, color=colors, capsize=5)
+
+# Achsenbeschriftungen und Titel
+plt.ylabel('nmol/min/µg')
+plt.xticks(np.arange(len(labels)), labels)
+
+# Pfad zum Speichern definieren
+save_path = 'D:/Platereader/Balkendiagramm&Fehlerbars.png'
+
+# Ordner erstellen, falls nicht vorhanden
+os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+# Plot speichern
+plt.savefig(save_path)
+
+# Plot anzeigen
+plt.show()
